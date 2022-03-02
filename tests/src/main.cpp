20fbd2b69842498528de06e444ae47392372160a
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-02 16:20:55 +0000

    Maintenance (#6)

---
+++ tests/src/main.cpp
@@ -0,0 +1,15 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    All rights reserved.
+*/
+
+#include <Ishiko/Tests.hpp>
+
+using namespace Ishiko::Tests;
+
+int main(int argc, char* argv[])
+{
+    TestHarness theTestHarness("IshikoHTTP");
+
+    return theTestHarness.run();
+}
