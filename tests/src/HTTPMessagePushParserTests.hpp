d8727cb5f49c46bbdeea3169c0ba85d4ff825c01
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-10 17:41:07 +0100

    Fix test code after change to dependencies (#32)

--- tests/src/HTTPMessagePushParserTests.hpp
+++ tests/src/HTTPMessagePushParserTests.hpp
@@ -6,7 +6,7 @@
 #ifndef _ISHIKO_CPP_HTTP_TESTS_HTTPMESSAGEPUSHPARSERTESTS_HPP_
 #define _ISHIKO_CPP_HTTP_TESTS_HTTPMESSAGEPUSHPARSERTESTS_HPP_
 
-#include <Ishiko/Tests/Core.hpp>
+#include <Ishiko/TestFramework/Core.hpp>
 
 class HTTPMessagePushParserTests : public Ishiko::TestSequence
 {
