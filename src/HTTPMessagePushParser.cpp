ed5049f2b7546e956d2c314c801f99b427064c94
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-20 00:07:06 +0100

    Parse responses (#38)

--- src/HTTPMessagePushParser.cpp
+++ src/HTTPMessagePushParser.cpp
@@ -5,13 +5,19 @@
 */
 
 #include "HTTPMessagePushParser.hpp"
+#include <Ishiko/Text.hpp>
 
-using namespace boost;
+using namespace Ishiko;
 
-namespace Ishiko
+void HTTPMessagePushParser::Callbacks::onRequest()
 {
+}
 
-void HTTPMessagePushParser::Callbacks::onMethod(string_view data)
+void HTTPMessagePushParser::Callbacks::onResponse()
+{
+}
+
+void HTTPMessagePushParser::Callbacks::onMethod(boost::string_view data)
 {
 }
 
@@ -23,21 +29,29 @@ void HTTPMessagePushParser::Callbacks::onHTTPVersion(boost::string_view data)
 {
 }
 
+void HTTPMessagePushParser::Callbacks::onStatusCode(boost::string_view data)
+{
+}
+
+void HTTPMessagePushParser::Callbacks::onReasonPhrase(boost::string_view data)
+{
+}
+
 void HTTPMessagePushParser::Callbacks::onHeader(boost::string_view name, boost::string_view value)
 {
 }
 
-void HTTPMessagePushParser::Callbacks::onBody(boost::string_view data)
+void HTTPMessagePushParser::Callbacks::onBodyFragment(boost::string_view data)
 {
 }
 
 HTTPMessagePushParser::HTTPMessagePushParser(Callbacks& callbacks)
-    : m_callbacks(callbacks)
+    : m_parsingMode(ParsingMode::methodOrHTTPVersion), m_callbacks(callbacks)
 {
 }
 
 // TODO: if pipelining is used then returning bool is not enough since we may have unused bytes at the end of data
-bool HTTPMessagePushParser::onData(string_view data)
+bool HTTPMessagePushParser::onData(boost::string_view data)
 {
     const char* previous = data.data();
     const char* current = previous;
@@ -48,14 +62,17 @@ bool HTTPMessagePushParser::onData(string_view data)
     {
         switch (m_parsingMode)
         {
-        case ParsingMode::method:
+        case ParsingMode::methodOrHTTPVersion:
             while (current < end)
             {
                 if (*current == ' ')
                 {
+                    // We have reached the end of the token without encountering a '/' so this a method and we are
+                    // parsing a request
+                    m_callbacks.onRequest();
                     if (m_fragmentedData1.empty())
                     {
-                        m_callbacks.onMethod(string_view(previous, (current - previous)));
+                        m_callbacks.onMethod(boost::string_view(previous, (current - previous)));
                     }
                     else
                     {
@@ -64,6 +81,13 @@ bool HTTPMessagePushParser::onData(string_view data)
                         m_fragmentedData1.clear();
                     }
                     m_parsingMode = ParsingMode::requestURI;
+                    break;
+                } 
+                else if (*current == '/')
+                {
+                    // Methods can't have a '/' in them so this is a protocol version and hence we are parsing a response
+                    m_callbacks.onResponse();
+                    m_parsingMode = ParsingMode::responseHTTPVersion;
                     break;
                 }
                 ++current;
@@ -86,7 +110,7 @@ bool HTTPMessagePushParser::onData(string_view data)
                 {
                     if (m_fragmentedData1.empty())
                     {
-                        m_callbacks.onRequestURI(string_view(previous, (current - previous)));
+                        m_callbacks.onRequestURI(boost::string_view(previous, (current - previous)));
                     }
                     else
                     {
@@ -94,7 +118,7 @@ bool HTTPMessagePushParser::onData(string_view data)
                         m_callbacks.onRequestURI(m_fragmentedData1);
                         m_fragmentedData1.clear();
                     }
-                    m_parsingMode = ParsingMode::httpVersion;
+                    m_parsingMode = ParsingMode::requestHTTPVersion;
                     break;
                 }
                 ++current;
@@ -109,7 +133,7 @@ bool HTTPMessagePushParser::onData(string_view data)
             }
             break;
 
-        case ParsingMode::httpVersion:
+        case ParsingMode::requestHTTPVersion:
             previous = current;
             while (current < end)
             {
@@ -117,12 +141,115 @@ bool HTTPMessagePushParser::onData(string_view data)
                 {
                     if (m_fragmentedData1.empty())
                     {
-                        m_callbacks.onHTTPVersion(string_view(previous, (current - previous)));
+                        m_callbacks.onHTTPVersion(boost::string_view(previous, (current - previous)));
                     }
                     else
                     {
                         m_fragmentedData1.append(data.data(), current - data.data());
                         m_callbacks.onHTTPVersion(m_fragmentedData1);
+                        m_fragmentedData1.clear();
+                    }
+                }
+                else if (*current == '\n')
+                {
+                    m_parsingMode = ParsingMode::headerOrSeparator;
+                    break;
+                }
+                ++current;
+            }
+            if (current == end)
+            {
+                // TODO: handle case where data size is 0
+                const char* adjustedCurrent = current;
+                if (*(current - 1) == '\r')
+                {
+                    --adjustedCurrent;
+                }
+                m_fragmentedData1.append(previous, (adjustedCurrent - previous));
+            }
+            else
+            {
+                ++current;
+            }
+            break;
+
+        case ParsingMode::responseHTTPVersion:
+            // We do not set the previous pointer here as the ParsingMode::methodOrHTTPVersion state handler has
+            // partially processed it
+            while (current < end)
+            {
+                if (*current == ' ')
+                {
+                    if (m_fragmentedData1.empty())
+                    {
+                        m_callbacks.onHTTPVersion(boost::string_view(previous, (current - previous)));
+                    }
+                    else
+                    {
+                        m_fragmentedData1.append(data.data(), current - data.data());
+                        m_callbacks.onHTTPVersion(m_fragmentedData1);
+                        m_fragmentedData1.clear();
+                    }
+                    m_parsingMode = ParsingMode::statusCode;
+                    break;
+                }
+                ++current;
+            }
+            if (current == end)
+            {
+                m_fragmentedData1.append(previous, (current - previous));
+            }
+            else
+            {
+                ++current;
+            }
+            break;
+
+        case ParsingMode::statusCode:
+            previous = current;
+            while (current < end)
+            {
+                if (*current == ' ')
+                {
+                    if (m_fragmentedData1.empty())
+                    {
+                        m_callbacks.onStatusCode(boost::string_view(previous, (current - previous)));
+                    }
+                    else
+                    {
+                        m_fragmentedData1.append(data.data(), current - data.data());
+                        m_callbacks.onStatusCode(m_fragmentedData1);
+                        m_fragmentedData1.clear();
+                    }
+                    m_parsingMode = ParsingMode::reasonPhrase;
+                    break;
+                }
+                ++current;
+            }
+            if (current == end)
+            {
+                m_fragmentedData1.append(previous, (current - previous));
+            }
+            else
+            {
+                ++current;
+            }
+            break;
+
+        case ParsingMode::reasonPhrase:
+            previous = current;
+            while (current < end)
+            {
+                if (*current == '\r')
+                {
+                    if (m_fragmentedData1.empty())
+                    {
+                        m_callbacks.onReasonPhrase(boost::string_view(previous, (current - previous)));
+                    }
+                    else
+                    {
+                        m_fragmentedData1.append(data.data(), current - data.data());
+                        m_callbacks.onReasonPhrase(m_fragmentedData1);
                         m_fragmentedData1.clear();
                     }
                 }
@@ -167,6 +294,7 @@ bool HTTPMessagePushParser::onData(string_view data)
             {
                 if (*current == ':')
                 {
+                    // TODO: I think there is an issue if we reach the end of the current data after we parse ":"
                     headerNameBegin = previous;
                     headerNameEnd = current;
                     m_parsingMode = ParsingMode::headerValue;
@@ -203,17 +331,49 @@ bool HTTPMessagePushParser::onData(string_view data)
                 {
                     if (m_fragmentedData1.empty() && m_fragmentedData2.empty())
                     {
-                        m_callbacks.onHeader(string_view(headerNameBegin, (headerNameEnd - headerNameBegin)),
-                            string_view(previous, (current - previous)));
+                        boost::string_view headerName(headerNameBegin, (headerNameEnd - headerNameBegin));
+                        boost::string_view headerValue(previous, (current - previous));
+                        if (headerName == "Content-Length")
+                        {
+                            // TODO: I actually want to use errors because we could be received lots of invalid
+                            // messages from misbehaved clients
+                            Error error;
+                            size_t contentLength = 0;
+                            ASCII::Convert(headerValue.begin(), headerValue.end(), contentLength, error);
+                            // TODO: handle error
+                            m_remainingContentLength = contentLength;
+                        }
+                        m_callbacks.onHeader(headerName, headerValue);
                     }
                     else if (!m_fragmentedData1.empty() && m_fragmentedData2.empty())
                     {
-                        m_callbacks.onHeader(m_fragmentedData1, string_view(previous, (current - previous)));
+                        boost::string_view headerValue(previous, (current - previous));
+                        if (m_fragmentedData1 == "Content-Length")
+                        {
+                            // TODO: I actually want to use errors because we could be received lots of invalid
+                            // messages from misbehaved clients
+                            Error error;
+                            size_t contentLength = 0;
+                            ASCII::Convert(headerValue.begin(), headerValue.end(), contentLength, error);
+                            // TODO: handle error
+                            m_remainingContentLength = contentLength;
+                        }
+                        m_callbacks.onHeader(m_fragmentedData1, headerValue);
                         m_fragmentedData1.clear();
                     }
                     else
                     {
                         m_fragmentedData2.append(data.data(), current - data.data());
+                        if (m_fragmentedData1 == "Content-Length")
+                        {
+                            // TODO: I actually want to use errors because we could be received lots of invalid
+                            // messages from misbehaved clients
+                            Error error;
+                            size_t contentLength = 0;
+                            ASCII::Convert(m_fragmentedData2.begin(), m_fragmentedData2.end(), contentLength, error);
+                            // TODO: handle error
+                            m_remainingContentLength = contentLength;
+                        }
                         m_callbacks.onHeader(m_fragmentedData1, m_fragmentedData2);
                         m_fragmentedData1.clear();
                         m_fragmentedData2.clear();
@@ -248,8 +408,14 @@ bool HTTPMessagePushParser::onData(string_view data)
             if ((current != end) && (*current == '\n'))
             {
                 ++current;
-                // TODO: handle the case where there is a body
-                return true;
+                if (current < end)
+                {
+                    m_parsingMode = ParsingMode::body;
+                }
+                else
+                {
+                    return true;
+                }
             }
             else
             {
@@ -259,11 +425,16 @@ bool HTTPMessagePushParser::onData(string_view data)
             break;
 
         case ParsingMode::body:
-            while (current < end)
+            if (m_remainingContentLength && (*m_remainingContentLength > 0))
             {
-                ++current;
+                // TODO: this is very problematic. The presence of a body may depend on the request the response is
+                // associated with e.g. HEAD. This means there is no point in having this generic parser. I may as well
+                // split into Request and Response parser and the response parser needs to have some info about which
+                // request it is associated with.
+                // TODO: take into account Content-Length
+                m_callbacks.onBodyFragment(boost::string_view(current, (end - current)));
             }
-            break;
+            return true;
         }
 
         // TODO: add an end state and error if trying to push more data
@@ -330,6 +501,4 @@ void HTTPMessagePushParser::notifyHeader()
             ++m_transferEncoding;
         }
     }*/
-}
-
 }
--- tests/src/helpers/HTTPMessagePushParserTestCallbacks.cpp
+++ tests/src/helpers/HTTPMessagePushParserTestCallbacks.cpp
@@ -5,7 +5,15 @@
 
 #include "HTTPMessagePushParserTestCallbacks.hpp"
 
-using namespace std;
+void HTTPMessagePushParserTestCallbacks::onRequest()
+{
+    m_messageType = MessageType::request;
+}
+
+void HTTPMessagePushParserTestCallbacks::onResponse()
+{
+    m_messageType = MessageType::response;
+}
 
 void HTTPMessagePushParserTestCallbacks::onMethod(boost::string_view data)
 {
@@ -22,27 +30,62 @@ void HTTPMessagePushParserTestCallbacks::onHTTPVersion(boost::string_view data)
     m_httpVersion = data.to_string();
 }
 
-void HTTPMessagePushParserTestCallbacks::onHeader(boost::string_view name, boost::string_view value)
+void HTTPMessagePushParserTestCallbacks::onStatusCode(boost::string_view data)
 {
-    m_headers.emplace_back(make_pair<string, string>(name.to_string(), value.to_string()));
+    m_statusCode = data.to_string();
 }
 
-const string HTTPMessagePushParserTestCallbacks::method() const
+void HTTPMessagePushParserTestCallbacks::onReasonPhrase(boost::string_view data)
+{
+    m_reasonPhrase = data.to_string();
+}
+
+void HTTPMessagePushParserTestCallbacks::onHeader(boost::string_view name, boost::string_view value)
+{
+    m_headers.emplace_back(std::make_pair<std::string, std::string>(name.to_string(), value.to_string()));
+}
+
+void HTTPMessagePushParserTestCallbacks::onBodyFragment(boost::string_view data)
+{
+    m_body.append(data.to_string());
+}
+
+HTTPMessagePushParserTestCallbacks::MessageType HTTPMessagePushParserTestCallbacks::messageType() const
+{
+    return m_messageType;
+}
+
+const std::string& HTTPMessagePushParserTestCallbacks::method() const
 {
     return m_method;
 }
 
-const string HTTPMessagePushParserTestCallbacks::requestURI() const
+const std::string& HTTPMessagePushParserTestCallbacks::requestURI() const
 {
     return m_requestURI;
 }
 
-const string HTTPMessagePushParserTestCallbacks::httpVersion() const
+const std::string& HTTPMessagePushParserTestCallbacks::httpVersion() const
 {
     return m_httpVersion;
 }
 
-const vector<pair<string, string>>& HTTPMessagePushParserTestCallbacks::headers() const
+const std::string& HTTPMessagePushParserTestCallbacks::statusCode() const
+{
+    return m_statusCode;
+}
+
+const std::string& HTTPMessagePushParserTestCallbacks::reasonPhrase() const
+{
+    return m_reasonPhrase;
+}
+
+const std::vector<std::pair<std::string, std::string>>& HTTPMessagePushParserTestCallbacks::headers() const
 {
     return m_headers;
+}
+
+const std::string& HTTPMessagePushParserTestCallbacks::body() const
+{
+    return m_body;
 }
