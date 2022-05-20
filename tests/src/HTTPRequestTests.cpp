250faa4149955291666f221c9970f12d481189d5
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-20 11:49:42 +0100

    Implement and test Connection header

--- tests/src/HTTPRequestTests.cpp
+++ tests/src/HTTPRequestTests.cpp
@@ -8,15 +8,14 @@
 #include <Ishiko/FileSystem.hpp>
 #include <string>
 
-using namespace boost::filesystem;
 using namespace Ishiko;
-using namespace std;
 
 HTTPRequestTests::HTTPRequestTests(const TestNumber& number, const TestContext& context)
     : TestSequence(number, "HTTPRequest tests", context)
 {
     append<HeapAllocationErrorsTest>("Constructor test 1", ConstructorTest1);
     append<FileComparisonTest>("toString test 1", ToStringTest1);
+    append<FileComparisonTest>("toString test 2", ToStringTest2);
 }
 
 void HTTPRequestTests::ConstructorTest1(Test& test)
@@ -30,10 +29,10 @@ void HTTPRequestTests::ConstructorTest1(Test& test)
 
 void HTTPRequestTests::ToStringTest1(FileComparisonTest& test)
 {
-    path outputPath(test.context().getTestOutputPath("HTTPRequestTests_ToStringTest1.bin"));
+    boost::filesystem::path outputPath(test.context().getTestOutputPath("HTTPRequestTests_ToStringTest1.bin"));
 
     HTTPRequest request(HTTPMethod::get, URL("http://example.org"));
-    string requestString = request.toString();
+    std::string requestString = request.toString();
 
     Error error;
     // TODO: should just throw an exception
@@ -45,6 +44,28 @@ void HTTPRequestTests::ToStringTest1(FileComparisonTest& test)
 
     test.setOutputFilePath(outputPath);
     test.setReferenceFilePath(test.context().getReferenceDataPath("HTTPRequestTests_ToStringTest1.bin"));
+
+    ISHIKO_TEST_PASS();
+}
+
+void HTTPRequestTests::ToStringTest2(FileComparisonTest& test)
+{
+    boost::filesystem::path outputPath(test.context().getTestOutputPath("HTTPRequestTests_ToStringTest2.bin"));
+
+    HTTPRequest request(HTTPMethod::get, URL("http://example.org"));
+    request.setConnectionHeader(HTTPHeader::ConnectionMode::keepAlive);
+    std::string requestString = request.toString();
+
+    Error error;
+    // TODO: should just throw an exception
+    BinaryFile file = BinaryFile::Create(outputPath, error);
+
+    ISHIKO_TEST_ABORT_IF(error);
+
+    file.write(requestString.c_str(), requestString.length());
+
+    test.setOutputFilePath(outputPath);
+    test.setReferenceFilePath(test.context().getReferenceDataPath("HTTPRequestTests_ToStringTest2.bin"));
 
     ISHIKO_TEST_PASS();
 }
